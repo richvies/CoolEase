fc08ca82a53d925ded077626195e6457c0144506
Richard Davies <richvies@gmail.com>
2020-12-25 20:41:10 +0100

    added bootloader utils header

--- common/include/common/bootloader_utils.h
+++ common/include/common/bootloader_utils.h
@@ -1,15 +1,58 @@
-/********************************************************************************
+/**
+ ******************************************************************************
  * @file    bootloader_utils.h
  * @author  Richard Davies
- * @brief   CoolEase Hub Main Function
- *
- * Function for jumping from bootloader code to application code.
+ * @date    25/Dec/2020
+ * @brief   Bootloader_utils Header File
+ *  
+ * @defgroup   BOOTLOADER_UTILS_FILE  Bootloader_utils
+ * @brief      
+ * 
+ * Description
+ * 
+ * @note     
+ * 
+ * @{
+ * @defgroup   BOOTLOADER_UTILS_API  Bootloader_utils API
+ * @brief      
+ * 
+ * @defgroup   BOOTLOADER_UTILS_INT  Bootloader_utils Internal
+ * @brief      
+ * @}
+ ******************************************************************************
  */
 
 #ifndef BOOTLOADER_UTILS_H
-#define BOOTLOADER_UTILS_H   
+#define BOOTLOADER_UTILS_H
+
+////////////////////////////////////////////////////////////////////////////////
+// Includes
+////////////////////////////////////////////////////////////////////////////////
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** @addtogroup BOOTLOADER_UTILS_API
+ * @{
+ */
+
+////////////////////////////////////////////////////////////////////////////////
+// Exported Variables
+////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Exported Function Declarations
+////////////////////////////////////////////////////////////////////////////////
 
 void boot_deinit();
 void boot_jump(uint32_t address);
 
+/** @} */
+
+#ifdef __cplusplus
+}
 #endif
+
+#endif /* BOOTLOADER_UTILS_H */
