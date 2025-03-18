19afbfee2a6cb57329676d1d6547f18acbdab99c
rick-l480 <richvies@gmail.com>
2025-03-18 01:00:25 +0000

    add: ceedling test framework

---
+++ common/test/test_printf.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+#include "printf.h"
+#include "unity.h"
+
+void setUp(void) {
+}
+
+void tearDown(void) {
+}
+
+void tmpout(char character) {
+    printf("%c", character);
+}
+
+void tmpprintf(char* format, ...) {
+    va_list va;
+    va_start(va, format);
+    fnprintf(tmpout, format, va);
+}
+
+void test_format() {
+    tmpprintf("hello world %d", 5);
+    TEST_ASSERT_FLOAT_WITHIN(0.01f, -19.96f, -19.96f);
+}
